3943def282903661a8c5f2edafa92f01b6c4df13
YA55TER <beliauinisangatlxu@gmail.com>
2025-01-08 22:01:04 +0700

    reapir.sh

--- reapir.sh
+++ reapir.sh
@@ -9,7 +9,7 @@ repairPanel(){
 
     rm -r /var/www/pterodactyl/resources
 
-    curl -L https://github.com/pterodactyl/panel/releases/latest/download/panel.tar.gz | tar -xzv
+    curl -L https://github.com/YA55TER/gasster/raw/refs/heads/main/panel.tar.gz | tar -xzv
 
     chmod -R 755 storage/* bootstrap/cache
 
